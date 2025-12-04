d6255a7fdb848a33880b3bfcef539d000ed80b31
SistemArte03 <asoportesistema03@gmail.com>
2025-12-03 21:18:35 -0500

    Update SecurityConfig.java

--- boterito/src/main/java/com/uptc/edu/boterito/config/SecurityConfig.java
+++ boterito/src/main/java/com/uptc/edu/boterito/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://192.168.1.244:5173"); // tu frontend
+        config.addAllowedOrigin("http://172.17.0.7:5173"); // tu frontend
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
