3c7c96f5055c8881b581af0af010d9186b3ec861
SistemArte03 <asoportesistema03@gmail.com>
2025-12-10 10:33:10 -0500

    Update SecurityConfig.java

--- boterito/src/main/java/com/uptc/edu/boterito/config/SecurityConfig.java
+++ boterito/src/main/java/com/uptc/edu/boterito/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.uptc.edu.boterito.security.CustomAuthEntryPoint;
 import com.uptc.edu.boterito.security.JwtAuthenticationFilter;
